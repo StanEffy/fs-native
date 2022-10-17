80bf06e8b1326669c7abb50260bfc963d5e4a06f
Stan Effi <holydonk@gmail.com>
2022-10-17 22:36:00 +0300

    10.8

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,10 +1,42 @@
-import {Text, View} from "react-native";
+import { Text, TextInput, Pressable, View } from 'react-native';
+import { Formik, useField } from 'formik';
+import FormikTextInput from "./FormikTextInput";
 
-const SignIn = () => {
+const initialValues = {
+    username: '',
+    password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+
     return (
         <View>
-            <Text>Sign in is here</Text>
+            <FormikTextInput
+                name={"username"}
+                placeholder="Username"
+
+            />
+            <FormikTextInput
+                name={"password"}
+                placeholder="Password"
+                secureTextEntry={true}
+
+            />
+            <Pressable onPress={onSubmit}>
+                <Text>Sign in</Text>
+            </Pressable>
         </View>
+    );
+};
+
+const SignIn = () => {
+    const onSubmit = (values) => {
+        console.log(values);
+    };
+    return (
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+            {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+        </Formik>
     );
 };
 
